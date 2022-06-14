988cdb104b9f5001c52824c7a9f06c084066fd07
DmitryOkay <okulik_dima@mail.ru>
2022-06-14 21:54:45 +0300

    Добавіл файл в нем 2 функціі на сравненіе строк по разным параметрам

---
+++ git1/2.py
@@ -0,0 +1,12 @@
+def str_simv(a='1234', b='45678'):
+    if a>b:
+        return a
+    else: return b
+print (str_simv())
+
+def str_len(a='1234', b='45678'):
+    if len(a)>len(b):
+
+        return a
+    else: return b
+print (str_len())
